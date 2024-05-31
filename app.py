053604d5479914612c560b2ac970d10a51937d4d
Maryia Kisialiova <maria_kisialiova@mail.ru>
2024-05-31 11:09:14 +0200

    Add user verification

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from models import db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '100aaa'
 db.init_app(app)
 
 with app.app_context():
@@ -20,6 +21,10 @@ def form():
     if request.method == 'POST':
         name = request.form['name']
         email = request.form['email']
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            flash('User with this email already exists', 'danger')
+            return render_template('form.html')
         new_user = User(name=name, email=email)
         db.session.add(new_user)
         db.session.commit()
